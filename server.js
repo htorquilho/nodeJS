7f188ff01c9261264e360382a1eac287a4760c33
Henrique Torquilho Almeida <henriquetorquilho@protonmail.com>
2023-09-18 20:28:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//importação do módulo http
 const http = require('http');
 
+//criação do servidor
 const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
     res.statusCode = 200;
     res.end('<h1>O Servidor tá ON!</h1>');
+    //manipulação de solicitações
 });
 
+//inicialização do servidor
 server.listen(3000, () => {
     console.log('Servidor funcionando')
 });
